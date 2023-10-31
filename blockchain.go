d93fa5dc694806000b9f4aa9cf10d0748d74aaa5
scarfacedotcom <peteradaaku@gmail.com>
2023-10-31 09:55:54 +0100

    new functions

--- blockchain.go
+++ blockchain.go
@@ -28,6 +28,23 @@ func (b *Block) Print() {
 	fmt.Printf("transaction %s\n", b.transaction)
 }
 
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "Init hash")
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
 func init() {
 	log.SetPrefix("Blockchain: ")
 
